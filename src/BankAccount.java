5a5dba4fb73a43201c0d38b239d80edc8c4dfbd2
masterchieftkd12 <masterchieftkd12@gmail.com>
2025-01-29 22:45:29 +0000

    hi

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        double number = 2175.37;
+        
+        number = number - 302.50;
+        
+        
+        number = number + 50.03;
+        
+        number = number / 2;
+        
+        number = number + 20;
+        
+        number = number - 1;
+        
+        number = number * 2;
+        
+        number = number + 1;
+        
+        System.out.printf("%.2f", number);
+        
+        
+    }
+}
 
